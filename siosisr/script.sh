3605ea051747015ac01593fe0bac66186da9285d
Luca <luca.bona06@gmail.com>
2025-11-05 11:12:40 +0100

    Correction du script.sh dans fx5

---
+++ siosisr/script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# script.sh - exemple de script simple
+
+echo "Bonjour, ceci est un script bash bidon !"
+
+# Afficher la date et l'heure
+echo "Nous sommes le : $(date)"
+
+# Afficher la liste des fichiers du répertoire courant
+echo "Voici les fichiers dans ce dossier :"
+ls -l
+
